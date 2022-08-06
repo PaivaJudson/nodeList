e04fbb9302b70f4b950e2669f3faec691ff67323
judsonpaiva <judson.paiva@hotmail.com>
2022-08-06 05:25:30 +0100

    acessando estilos pelo JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,14 @@
 
 const paragrafos = document.querySelector('.paragrafos');
 const listaParagrafos = paragrafos.querySelectorAll('p');
+
+const estilosBody = getComputedStyle(document.body);
+const backgroundColorBody = estilosBody.backgroundColor;
+
+console.log(backgroundColorBody);
+
+for (let p of listaParagrafos) {
+    p.style.backgroundColor = backgroundColorBody;
+    p.style.color = '#FFF';
+    console.log(p);
+}
